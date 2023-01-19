410ea29cf3ab144364789cb3bd64a4f29f164061
chayaestherlangsner <celangsner@gmail.com>
2023-01-19 01:47:52 -0500

    1st commit for trial

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -1,8 +1,11 @@
 use humanbodydb
 go 
+drop table if exists body 
+go
 create table dbo.body
 (
     id int not null identity primary key, 
+	creaturetype varchar(25) not null,
     bodypart varchar(100) not null, 
     num int not null
 )
